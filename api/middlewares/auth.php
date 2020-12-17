0ccc1992528c034320721a4a644e27f5fdeb7a17
NozomuTsuruta <tuno.0806@icloud.com>
2020-12-17 17:46:17 +0900

    phpのコード整理

--- api/middlewares/auth.php
+++ api/middlewares/auth.php
@@ -17,53 +17,52 @@ class Auth extends JwtHandler
     {
 
         if (array_key_exists('Authorization', $this->headers) && !empty(trim(
-            $this->headers['Authorization']))):
+            $this->headers['Authorization']))) {
             $this->token = explode(' ', trim($this->headers['Authorization']));
 
-            if (isset($this->token[1]) && !empty(trim($this->token[1]))):
+            if (isset($this->token[1]) && !empty(trim($this->token[1]))) {
 
                 $data = $this->_jwt_decode_data($this->token[1]);
 
                 if (isset($data['auth']) && isset($data['data']->user_id) &&
-            $data['auth']):
+                    $data['auth']) {
                     $user = $this->fetchUser($data['data']->user_id);
                     return $user;
 
-                else:
+                } else {
                     return null;
+                }
 
-                endif; // End of isset($this->token[1]) && !empty(trim($this->token[1]))
-
-            else:
+            } else {
                 return null;
 
-            endif; // End of isset($this->token[1]) && !empty(trim($this->token[1]))
+            }
 
-        else:
+        } else {
             return null;
+        }
 
-        endif;
     }
 
     protected function fetchUser($user_id)
     {
         try {
-            $fetch_user_by_id =
-                'SELECT `name`,`email` FROM `users` WHERE `id`=:id';
-            $query_stmt = $this->db->prepare($fetch_user_by_id);
-            $query_stmt->bindValue(':id', $user_id, PDO::PARAM_INT);
+            $query_stmt = $this->db->prepare(
+                'SELECT name,email FROM users WHERE id=?');
+            $query_stmt->bindValue(1, $user_id, PDO::PARAM_INT);
             $query_stmt->execute();
 
-            if ($query_stmt->rowCount()):
+            if ($query_stmt->rowCount()) {
                 $row = $query_stmt->fetch(PDO::FETCH_ASSOC);
                 return [
                     'success' => 1,
                     'status' => 200,
                     'user' => $row,
                 ];
-            else:
+            } else {
                 return null;
-            endif;
+            }
+
         } catch (PDOException $e) {
             return null;
         }
--- api/signin.php
+++ api/signin.php
@@ -24,59 +24,53 @@ $db_connection = new Database();
 $conn = $db_connection->db_connection();
 
 $data = json_decode(file_get_contents('php://input'));
-$returnData = [];
+$return_data = [];
 
 // IF REQUEST METHOD IS NOT EQUAL TO POST
-if ($_SERVER['REQUEST_METHOD'] != 'POST'):
-    $returnData = msg(0, 404, 'Page Not Found!');
+if ($_SERVER['REQUEST_METHOD'] != 'POST') {
+    $return_data = msg(0, 404, 'Page Not Found!');
 
 // CHECKING EMPTY FIELDS
-elseif (!isset($data->email)
-    || !isset($data->password)
-    || empty(trim($data->email))
-    || empty(trim($data->password))
-):
+} elseif (!isset($data->email) || !isset($data->password) || empty(trim($data->
+        email)) || empty(trim($data->password))) {
 
     $fields = ['fields' => ['email', 'password']];
     $returnData = msg(0, 422, 'Please Fill in all Required Fields!', $fields);
 
 // IF THERE ARE NO EMPTY FIELDS THEN-
-else:
+} else {
     $email = trim($data->email);
     $password = trim($data->password);
 
 // CHECKING THE EMAIL FORMAT (IF INVALID FORMAT)
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)):
-        $returnData = msg(0, 422, 'Invalid Email Address!');
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $return_data = msg(0, 422, 'Invalid Email Address!');
 
         // IF PASSWORD IS LESS THAN 8 THE SHOW THE ERROR
-    elseif (strlen($password) < 8):
-        $returnData = msg(0, 422,
+    } elseif (strlen($password) < 8) {
+        $return_data = msg(0, 422,
             'Your password must be at least 8 characters long!');
 
         // THE USER IS ABLE TO PERFORM THE LOGIN ACTION
-    else:
+    } else {
         try {
-
-            $fetch_user_by_email = 'SELECT * FROM `users` WHERE `email`=:email'
-            ;
-            $query_stmt = $conn->prepare($fetch_user_by_email);
-            $query_stmt->bindValue(':email', $email, PDO::PARAM_STR);
+            $query_stmt = $conn->prepare('SELECT * FROM users WHERE email=?');
+            $query_stmt->bindValue(1, $email, PDO::PARAM_STR);
             $query_stmt->execute();
 
 // IF THE USER IS FOUNDED BY EMAIL
-            if ($query_stmt->rowCount()):
+            if ($query_stmt->rowCount()) {
                 $row = $query_stmt->fetch(PDO::FETCH_ASSOC);
                 $check_password = password_verify($password, $row['password']);
 
 // VERIFYING THE PASSWORD (IS CORRECT OR NOT?)
 
 // IF PASSWORD IS CORRECT THEN SEND THE LOGIN TOKEN
-                if ($check_password):
+                if ($check_password) {
 
                     $jwt = new JwtHandler();
                     $token = $jwt->_jwt_encode_data(
-                        'http://localhost/php_auth_api/',
+                        'http://localhost/',
                         ['user_id' => $row['id']]
                     );
 
@@ -85,22 +79,22 @@ else:
                         'message' => 'You have successfully logged in.',
                         'token' => $token,
                     ];
-
                     // IF INVALID PASSWORD
-                else:
+                } else {
                     $returnData = msg(0, 422, 'Invalid Password!');
-                endif;
+                }
 
                 // IF THE USER IS NOT FOUNDED BY EMAIL THEN SHOW THE FOLLOWING ERROR
-            else:
+            } else {
                 $returnData = msg(0, 422, 'Invalid Email Address!');
-            endif;
+            }
+
         } catch (PDOException $e) {
             $returnData = msg(0, 500, $e->getMessage());
         }
 
-    endif;
+    }
 
-endif;
+}
 
 echo json_encode($returnData);
